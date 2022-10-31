3e648acdc8d7123b273e21547094e9ddc3d049ec
Talita Saez <tltsaez@gmail.com>
2022-10-31 16:22:57 -0300

    req 37

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Recipes from './pages/Recipes';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 import './App.css';
 
@@ -21,8 +22,16 @@ function App() {
             <Route exact path="/drinks" component={ Recipes } />
             <Route exact path="/meals/:id" component={ RecipeDetails } />
             <Route exact path="/drinks/:id" component={ RecipeDetails } />
-            <Route exact path="/meals/:id-da-receita/in-progress" />
-            <Route exact path="/drinks/:id-da-receita/in-progress" />
+            <Route
+              exact
+              path="/meals/:id/in-progress"
+              component={ RecipeInProgress }
+            />
+            <Route
+              exact
+              path="/drinks/:id/in-progress"
+              component={ RecipeInProgress }
+            />
             <Route path="/done-recipes" component={ DoneRecipes } />
             <Route path="/favorite-recipes" component={ FavoriteRecipes } />
             <Route path="/profile" component={ Profile } />
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,45 +1,33 @@
-import React from 'react';
-// import RecipesContext from '../Context/recipesContext';
-// comentario
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import getRecipes from '../helpers/getRecipes';
+import RecipeDetailCard from '../Components/RecipeDetailCard';
+
 function RecipeInProgress() {
+  const { id } = useParams();
+  const [details, setDetails] = useState({});
+  // const [srcVideo, setSrcVideo] = useState('');
+  const history = useHistory();
+  const path = history.location.pathname;
+  const SIX = 6;
+  const deleteIdFromPath = path.substring(0, SIX);
+
+  useEffect(() => {
+    async function fetchData() {
+      const detailsToRender = await getRecipes('details', id, deleteIdFromPath);
+      setDetails(detailsToRender[0]);
+      if (path.includes('/meals')) {
+        setSrcVideo(detailsToRender[0]);
+      }
+    }
+    fetchData();
+  }, [deleteIdFromPath, id, path]);
+
+  const { idMeal, idDrink } = details;
   return (
 
-    <div>
-
-      <img data-testid="recipe-photo" src="" alt="" />
-
-      <h2 data-testid="recipe-title"> Título </h2>
-
-      <button
-        type="button"
-        data-testid="share-btn"
-      >
-        Compartilhar
-      </button>
-
-      <button
-        type="button"
-        data-testid="favorite-btn"
-      >
-        Favoritar
-      </button>
-
-      <h3 data-testid="recipe-category">
-        Texto Categoria
-      </h3>
-
-      <p data-testid="instructions">Instruções</p>
-
-      <button
-        type="button"
-        data-testid="finish-recipe-btn"
-
-      >
-        Finalizar
-      </button>
-
-    </div>
-  );
-}
+  
+  
+)
 
 export default RecipeInProgress;
