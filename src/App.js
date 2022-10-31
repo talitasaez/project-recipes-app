0c0f9a55cc282a211252a3df82df72f0f682c2b3
JoaoGabrielVargas <106772621+JoaoGabrielVargas@users.noreply.github.com>
2022-10-31 11:06:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,12 +19,13 @@ function App() {
             <Route exact path="/" component={ Login } />
             <Route exact path="/meals" component={ Recipes } />
             <Route exact path="/drinks" component={ Recipes } />
-            <Route path="/meals/:id" component={ RecipeDetails } />
-            <Route path="/drinks/:id" component={ RecipeDetails } />
-            <Route path="/meals/:id-da-receita/in-progress" />
-            <Route path="/drinks/:id-da-receita/in-progress" />
+            <Route exact path="/meals/:id" component={ RecipeDetails } />
+            <Route exact path="/drinks/:id" component={ RecipeDetails } />
+            <Route exact path="/meals/:id-da-receita/in-progress" />
+            <Route exact path="/drinks/:id-da-receita/in-progress" />
             <Route path="/done-recipes" component={ DoneRecipes } />
             <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+            <Route path="/profile" component={ Profile } />
           </Switch>
         </BrowserRouter>
       </RecipesProvider>
